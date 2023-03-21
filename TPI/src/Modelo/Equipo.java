4a4910455e6f82aa9d7345fb82b36e38bae001d9
lautaroxz <lauti1998@hotmail.com>
2023-03-21 12:12:40 -0300

    metodo de resultado

--- TPI/src/Modelo/Equipo.java
+++ TPI/src/Modelo/Equipo.java
@@ -4,5 +4,12 @@ public class Equipo {
 	
 	private String equipo;
 	private int goles;
-
+    
+	public Equipo{
+	}
+	
+	public Equipo(String nombre; int cantgoles ){
+		this.equipo=nombre;
+		this.goles=cantgoles;
+	}
 }
--- TPI/src/Modelo/Resultado.java
+++ TPI/src/Modelo/Resultado.java
@@ -7,6 +7,19 @@ public class Resultado {
 	private int cantGoles1;
 	private int cantGoles2;
 
+	public Resultado(int goles1; int goles2){
+		this.cantGoles1=goles1;
+		this.cantGoles2=goles2;
+	}
+	if(cantGoles1>cantGoles2) {
+		return equipo1;
+	}else {
+	   if(cantGoles1<cantGoles2) {
+	   return equipo2;
+	   else {
+	   return 0;
+	   }
+	   }
+	}
 	
-
 }
